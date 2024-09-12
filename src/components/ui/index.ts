ac00f7b3cade56c8d699fe8c9d29436792a0ff7d
Shubham Prakash <shubhamprakash681@gmail.com>
2024-09-12 23:01:53 +0530

    Real Time Text Editor (RTE) component added

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,5 +3,6 @@ import Logo from "./Logo";
 import Button from "./Button";
 import Input from "./Input";
 import Select from "./Select";
+import RTE from "./rte/RTE";
 
-export { Button, Container, Input, Logo, Select };
+export { Button, Container, Input, Logo, Select, RTE };
---
+++ src/components/ui/rte/RTE.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import BundledEditor from "./BundledEditor";
+import { Control, Controller } from "react-hook-form";
+
+type RTEProps = {
+  name: string;
+  control: Control;
+  label: string;
+  defaultValue: string;
+};
+
+const RTE: React.FC<RTEProps> = ({ name, control, label, defaultValue }) => {
+  return (
+    <div className="w-full">
+      {label && <label className="inline-block mb-1 pl-1">{label}</label>}
+
+      <Controller
+        name={name || "content"}
+        control={control}
+        render={({ field: { onChange } }) => (
+          <BundledEditor
+            initialValue={defaultValue}
+            onEditorChange={onChange}
+            init={{
+              height: 500,
+              menubar: false,
+              plugins: [
+                "advlist",
+                "anchor",
+                "autolink",
+                "help",
+                "image",
+                "link",
+                "lists",
+                "searchreplace",
+                "table",
+                "wordcount",
+              ],
+              toolbar:
+                "undo redo | blocks | " +
+                "bold italic forecolor | alignleft aligncenter " +
+                "alignright alignjustify | bullist numlist outdent indent | " +
+                "removeformat | help",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+            }}
+          />
+        )}
+      />
+    </div>
+  );
+};
+
+export default RTE;
