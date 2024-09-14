2d10a4d4715b741457aa9d5a17b007b54a480024
Shubham Prakash <shubhamprakash681@gmail.com>
2024-09-14 23:57:23 +0530

    Logo style changes

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -3,10 +3,11 @@ import React from "react";
 type LogoProps = {
   children?: React.ReactNode;
   width?: string;
+  [key: string]: any;
 };
 
-const Logo: React.FC<LogoProps> = ({ children, width = "100px" }) => {
-  return <div>{children}</div>;
+const Logo: React.FC<LogoProps> = ({ children, width = "100px", ...props }) => {
+  return <div {...props}>{children}</div>;
 };
 
 export default Logo;
