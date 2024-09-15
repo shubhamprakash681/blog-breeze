9dfd252b9824d835be24629ce8a52276f03896c9
Shubham Prakash <shubhamprakash681@gmail.com>
2024-09-15 20:25:18 +0530

    Page container enhanced

--- src/components/ui/PageContainer.tsx
+++ src/components/ui/PageContainer.tsx
@@ -2,10 +2,20 @@ import React from "react";
 
 type PageContainerProps = {
   children?: React.ReactNode;
+  className?: string;
+  [key: string]: any;
 };
 
-const PageContainer: React.FC<PageContainerProps> = ({ children }) => {
-  return <div className="page-container p-6">{children}</div>;
+const PageContainer: React.FC<PageContainerProps> = ({
+  children,
+  className,
+  ...props
+}) => {
+  return (
+    <div {...props} className={`${className} page-container p-6`}>
+      {children}
+    </div>
+  );
 };
 
 export default PageContainer;
